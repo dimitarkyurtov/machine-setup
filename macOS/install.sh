d35cfbb3eeb7b64d496cb6fb31960380d9646dfc
dimitarkyurtov <dimitarkiurtov@gmail.com>
2026-05-24 18:26:34 +0300

    Created macOS install script

---
+++ macOS/install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Ghostty terminal emulator - https://ghostty.org/docs
+brew install --cask ghostty
+
+# Fuzzy finder - https://github.com/junegunn/fzf
+brew install fzf
+
+# Starship prompt - https://starship.rs
+brew install starship
